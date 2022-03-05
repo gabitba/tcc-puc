92ee82963ecac22225f437982c92bcd60cef5599
gabrielatba <gabrielatba@hotmail.com>
2022-03-05 19:25:02 -0300

    Implement local database with docker and run migration.

---
+++ ModuloInformacoesCadastrais/ModuloInformacoesCadastrais.Infra.Data/Database/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ModuloInformacoesCadastrais.Domain.Entidades;
+using System.Data.Entity;
+
+namespace ModuloInformacoesCadastrais.Infra.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(string connectionString) : base(connectionString){ }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ ModuloInformacoesCadastrais/ModuloInformacoesCadastrais.Infra.Data/Database/AppDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using ModuloInformacoesCadastrais.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloInformacoesCadastrais.Infra.Data.Database
+{
+    internal class AppDbContextFactory : IDbContextFactory<AppDbContext>
+    {
+        //TODO put connection string
+        public AppDbContext Create()
+        {
+            return new AppDbContext("Data Source=host.docker.internal,1433;Initial Catalog=Master;User ID=sa;Password=Gabs!Tcc");
+        }
+    }
+}
