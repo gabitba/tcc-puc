b10fa62673a928a63cf7113a1a044da300771698
Gabriela Tolentino Boaventura de Almeida <gabrielatba@hotmail.com>
2022-03-20 17:25:23 -0300

    Implementando terraform

--- devops/terraform/main.tf
+++ devops/terraform/main.tf
@@ -1,8 +1,20 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "TCC-PUC"
+    storage_account_name = "tccpuc"
+    container_name       = "terraformstate"
+    key                  = "prod.terraform.tfstate"
+    client_id            = "b5acc16c-d659-47cc-9b41-0f1730ea14d5"
+    client_secret        = "B-G7Q~0fXurm9CDTNYUelbSnAOxeOh5eI~fvy"
+    subscription_id      = "f63ea9a1-6b21-4536-a566-b7e50aee50cb"
+    tenant_id            = "b56bdd3f-e9bc-4072-bab4-2191ce42dc0e"
+    use_microsoft_graph  = true
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.65"
+      version = "=2.99.0"
     }
   }
 
@@ -10,5 +22,11 @@ terraform {
 }
 
 provider "azurerm" {
+  skip_provider_registration = true
+  use_msal                   = true
   features {}
+}
+
+data "azurerm_resource_group" "tccpuc" {
+  name = "TCC-PUC"
 }
