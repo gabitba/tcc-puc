6dec998c3540c5bc27135ad6df54f8a885c01387
gabrielatba <gabrielatba@hotmail.com>
2022-04-08 09:02:54 -0300

    Continue development of use-case-3

--- PortalCorporativo/portal-corporativo/src/app/app.module.ts
+++ PortalCorporativo/portal-corporativo/src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
+  import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClientesComponent } from './clientes/clientes.component';
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
