0e7b161cb5b31b9c156497377c5efa20632851c3
gabrielatba <gabrielatba@hotmail.com>
2022-04-13 12:41:36 -0300

    Enrich error

--- ModuloInformacoesCadastrais/ModuloInformacoesCadastrais.API/Controllers/BaseController.cs
+++ ModuloInformacoesCadastrais/ModuloInformacoesCadastrais.API/Controllers/BaseController.cs
@@ -13,8 +13,10 @@ namespace ModuloInformacoesCadastrais.API.Controllers
 
         protected ObjectResult HandleError(Exception ex)
         {
-            logger.LogError(ex.Message, ex.StackTrace);
-            return StatusCode(500, "Erro inesperado.");
+            logger.LogError(ex, ex.Message);
+
+            //Nunca deve expor detalhes do erro para o cliente. Só foi feito aqui por questões de ser uma aplicação interna para prototipação. 
+            return StatusCode(500, $"Erro inesperado. Error: {ex.Message}");
         }
     }
 }
