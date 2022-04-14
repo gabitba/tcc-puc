d2c52f8fbfad25d89a360f2d214a6ef37fedf1b2
gabrielatba <gabrielatba@hotmail.com>
2022-04-14 15:33:20 -0300

    Renamed roles

--- ModuloInformacoesCadastrais/ModuloInformacoesCadastrais.API/Authorization/ReadAndWriteProtectedResource.cs
+++ ModuloInformacoesCadastrais/ModuloInformacoesCadastrais.API/Authorization/ReadAndWriteProtectedResource.cs
@@ -4,8 +4,8 @@ namespace ModuloInformacoesCadastrais.API.Authorization
     {
         public string ResourceName { get; private set; }
         public string ResourceSuffix { get; private set; }
-        public string Read { get => $"{this.ResourceName}.Read{this.ResourceSuffix}"; }
-        public string Write { get => $"{this.ResourceName}.Write{this.ResourceSuffix}"; }
+        public string Read { get => $"{this.ResourceName}.Reader{this.ResourceSuffix}"; }
+        public string Write { get => $"{this.ResourceName}.Writer{this.ResourceSuffix}"; }
 
         public ReadAndWriteProtectedResource(string resourceName, string? resourceSuffix = null)
         {
